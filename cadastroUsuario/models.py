1491f246800030313dc774377c6c1c73c58667a7
annecaroli <carolinesilva4@hotmail.com>
2023-06-27 17:59:26 -0300

    Modelagem de dados

--- cadastroUsuario/models.py
+++ cadastroUsuario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CadastroUsuario(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cpf = models.CharField(max_length=11, null=False, blank=False)
+    login = models.CharField(max_length=15, null=False, blank=False)
+    senha = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
--- cadastroUsuario/views.py
+++ cadastroUsuario/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from cadastroUsuario.models import CadastroUsuario
+
 
 def index(request):
-    return render(request, 'cadastroUsuario/index.html')
+    users = CadastroUsuario.objects.all()
+    return render(request, 'cadastroUsuario/index.html', {"cards": users})
+
+def usuario(request, user_id):
+    user = get_object_or_404(CadastroUsuario, pk=user_id)
+    return render(request, 'user.html', {'user': user})
